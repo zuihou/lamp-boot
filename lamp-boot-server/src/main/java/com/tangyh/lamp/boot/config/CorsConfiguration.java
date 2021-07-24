9e9c0df65884ccef2ad185d94e5bd858fcdf739c
zuihou <244387066@qq.com>
2021-07-24 12:08:18 +0800

    fix: 跨域问题

--- lamp-boot-server/src/main/java/com/tangyh/lamp/boot/config/CorsConfiguration.java
+++ lamp-boot-server/src/main/java/com/tangyh/lamp/boot/config/CorsConfiguration.java
@@ -24,7 +24,7 @@ public class CorsConfiguration {
         // 允许cookies跨域
         config.setAllowCredentials(true);
         // #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         // #允许访问的头信息,*表示全部
         config.addAllowedHeader("*");
         // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
